990d73a10d19f00d7c98081b07cbe49ad3a2ecfc
Aman Sahni <6264069a@gmail.com>
2026-09-10 15:43:10 +0530

    create table for bus booking system

--- index.js
+++ index.js
@@ -15,21 +15,37 @@ connection.connect((err) =>{
         return;
     }
 
-    console.log("Mysql is connected")
+    try{
+        const createUser = `create table Users(
+                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                name VARCHAR(20),
+                                email VARCHAR(30)
+                            )`;
+        const Buses = `create table Buses(
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            busNumber INT,
+                            totalSeats INT,
+                            availableSeats INT
+                        )`;
+        const Booking = `create table Booking(
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            seatNumber INT
+                        )`;
+        const payment = `create table Payment(
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            amountPaid INT,
+                            paymentStatus BOOLEAN
+                        )`;
 
-    const firstQuery = `create table student(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(20),
-        email VARCHAR(30)
-    )`
+        connection.execute(createUser);
+        connection.execute(Buses);
+        connection.execute(Booking);
+        connection.execute(payment);
 
-    connection.execute(firstQuery,(err) =>{
-        if(err){
-            console.log(err)
-        }
+    }catch(err){
+        console.log(err)
+    }
 
-        console.log("Table is created")
-    });
 })
 
 app.get("/",(req,res) =>{
